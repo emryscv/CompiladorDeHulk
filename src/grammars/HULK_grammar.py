c5355898fcaada917d259ca3b7602c1267dda7ef
emryscv <emryscv@gmail.com>
2024-06-13 10:09:27 -0400

    feat/var, if-else, loops gramar

--- src/grammars/HULK_grammar.py
+++ src/grammars/HULK_grammar.py
@@ -5,12 +5,14 @@ from ast_nodes.hulk_ast_nodes import *
 def get_hulk_grammar():
     G = Grammar()
     
-    expr = G.NonTerminal('<expr>', startSymbol=True)
-    expr_list, stringify, term, factor, atom, func_call, arg_list = G.NonTerminals('<expr-list> <stringify> <term> <factor> <atom> <func-call> <arg-list>')
+    expr_or_block = G.NonTerminal('<expr-or-block>', startSymbol=True)
+    expr, expr_list, stringify, term, factor, atom, func_call, arg_list, func_dec, arg_dec_list, func_body, var_def, boolean_expr, boolean_term = G.NonTerminals('<expr> <expr-list> <stringify> <term> <factor> <atom> <func-call> <arg-list> <func-dec> <arg-dec-list> <func-body> <var-def> <boolean-expr> <boolean-term>')
     
-    sum, sub, mul, div, pow1, pow2, num, id, opar, cpar, ocurl, ccurl, coma, semicolon, at = G.Terminals('+ - * / ^ ** num id ( ) { } , ; @')
+    sum, sub, mul, div, pow1, pow2, num, id, opar, cpar, ocurl, ccurl, coma, semicolon, at, function, arrow, let, in_token, asign_equal, asign, if_token, else_token, and_token, or_token, lower, greater, lower_equal, greater_equal, equal, diferent, true, false, while_token, for_token = G.Terminals('+ - * / ^ ** num id ( ) { } , ; @ function => let in = := if else & | < > <= >= == != true false while for')
     
-    expr %= ocurl + expr_list + ccurl, lambda h, s: BlockExprNode(s[2]), None, None, None
+    expr_or_block %= expr
+    expr_or_block %= ocurl + expr_list + ccurl, lambda h, s: BlockExprNode(s[2]), None, None, None
+    
     expr %= stringify + at + expr, lambda h, s: BinaryOperationNode(s[1], s[3], s[2]), None, None, None
     expr %= stringify, lambda h, s: s[1], None
     
@@ -38,5 +40,45 @@ def get_hulk_grammar():
     
     arg_list %= expr + coma + arg_list, lambda h, s: [s[1]] + s[3], None, None, None
     arg_list %= expr, lambda h, s: [s[1]], None
+    
+    
+    ###functions###
+    
+    func_dec %= function + id + opar + arg_dec_list + cpar + func_body, None, None, None, None, None, None, None
+    func_body %= arrow + expr + semicolon, None, None, None, None
+    func_body %= ocurl + expr_list + ccurl, None, None, None, None
+    
+    ###variables###
+    
+    
+    expr %= let + var_def + in_token + expr_or_block, None, None, None, None
+    expr %= id + asign + expr_or_block
+    
+    var_def %= id + asign_equal + expr_or_block + coma + var_def
+    var_def %= id + asign_equal + expr_or_block
+    
+    ### if - else###
+ 
+    expr %= if_token + opar + boolean_expr + cpar + expr_or_block + else_token + expr_or_block
+    
+    boolean_expr %= boolean_term + and_token + boolean_expr
+    boolean_expr %= boolean_term + or_token + boolean_expr
+    boolean_expr %= boolean_term
+    
+    boolean_term %= expr + lower + expr, None, None, None, None
+    boolean_term %= expr + greater + expr, None, None, None, None
+    boolean_term %= expr + lower_equal + expr, None, None, None, None
+    boolean_term %= expr + greater_equal + expr, None, None, None, None
+    boolean_term %= expr + equal + expr, None, None, None, None
+    boolean_term %= expr + diferent + expr, None, None, None, None
+    boolean_term %= true, None, None
+    boolean_term %= false, None, None
+    boolean_term %= id, None, None
+    
+    
+    ###loops###
+    expr %= while_token + opar + boolean_expr + cpar + expr_or_block, None, None, None, None, None, None
+    expr %= for_token + opar + id + in_token + expr_or_block + cpar + expr_or_block, None, None, None, None, None, None, None, None
+    
     
     return G
