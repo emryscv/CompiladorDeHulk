da63941596f30b1f9ea5404c180c94802f244b03
emryscv <emryscv@gmail.com>
2024-07-05 10:08:21 -0400

    feat/value to Variable Class

--- src/semantic_check/utils/Variable.py
+++ src/semantic_check/utils/Variable.py
@@ -1,7 +1,8 @@
 class Variable:
-    def __init__(self, name, vtype):
+    def __init__(self, name, vtype, value=None):
         self.name = name
         self.vtype = vtype
+        self.value = value
         
     def __str__(self):
         return f'{self.name}:{self.vtype}'
