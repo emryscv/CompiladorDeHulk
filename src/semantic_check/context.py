8210e31d3501ed382cda50e06bbbae113ee23756
emryscv <emryscv@gmail.com>
2024-07-06 11:03:19 -0400

    feat/Protocol to context

--- src/semantic_check/context.py
+++ src/semantic_check/context.py
@@ -1,15 +1,16 @@
 from semantic_check.utils.Type import Type
+from semantic_check.utils.Protocol import Protocol
 
 class Context:
     def __init__(self):
-        self.types = {
-            "Object" : Type("Object"), 
+        self.types: dict[str, Type]= {
+            "Object": Type("Object"), 
             "String": Type("String"),
             "Number": Type("Number"),
             "Boolean": Type("Boolean"),
         }
-        self.protocols = {
-            
+        self.protocols: dict[str, Protocol] = {
+            "Iterable": Type("Iterable") #TODO añadirle los metodos q hacen falta
         }
         
         object = self.get_type("Object")
@@ -18,10 +19,20 @@ class Context:
         self.get_type("Boolean").parent = object
 
     def create_type(self, name:str):
-        if name in self.types:
-            return [f'Type with the same name ({name}) already in context.']   
-        self.types[name] = Type(name)
-        return []
+        message = self.is_defined(name)
+        
+        if len(message) == 0:
+            self.types[name] = Type(name)
+        
+        return message
+    
+    def create_protocol(self, name:str):
+        message = self.is_defined(name)
+        
+        if len(message) == 0:
+            self.types[name] = Protocol(name)
+        
+        return message
         
     def is_type_defined(self, name:str):
         if name in self.types:
@@ -29,20 +40,23 @@ class Context:
         
         return (f'Type "{name}" is not defined.')
     
-    def get_type(self, name:str):
-        return self.types[name]
-    
-    def create_protocol(self, name:str):
-        if name in self.protocols:
-            return [f'Protocol with the same name ({name}) already in context.']   
-        self.types[name] = Type(name)
-        return []
-        
     def is_protocol_defined(self, name:str):
-        if name in self.types:
+        if name in self.protocols:
             return []
         
-        return (f'Type "{name}" is not defined.')
+        return (f'Protocol "{name}" is not defined.')
+    
+    def is_defined(self, name:str):
+        if name in self.types:
+            return [f'Type with the same name ({name}) already in context.']
+        
+        if name in self.protocols:
+            return [f'Protocol with the same name ({name}) already in context.']
+        
+        return []
+    
+    def get_type(self, name:str):
+        return self.types[name]
     
     def get_protocol(self, name:str):
         return self.types[name]
